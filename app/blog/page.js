e75bd0098cedbd2f4b09d6094edbdcb6c65a9a23
Devilxz9 <pk675061@gmail.com>
2025-12-24 20:55:14 +0530

    fix: production env vars and fetch urls

--- app/blog/page.js
+++ app/blog/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Image from 'next/image'
 
 async function getdata() {
-  const res = await fetch('http://localhost:3000/api/posts', { cache: 'no-store',});
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/posts`, { cache: 'no-store',});
 
   if (!res.ok){
     throw new Error('failed to fetch data')
